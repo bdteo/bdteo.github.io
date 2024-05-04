fe23472a5dc9c9b62a3da422438364195c40e0ee
Boris D. Teoharov <boristeoharov@gmail.com>
2024-05-04 13:15:01 +0300

    Update blog details and exclude directories in project settings
    Updated gatsby-config.js to reflect the new blog title and icon. Also, added 'public' and '.cache' to the excluded folders list in the project settings.
    
    The new details now reflect Boris D. Teoharov's blog instead of 'Gatsby Starter Blog'. In the project settings, 'public' and '.cache' folders are now added to 'excludeFolder' to avoid tracking unnecessary files.

--- gatsby-config.js
+++ gatsby-config.js
@@ -103,7 +103,7 @@ module.exports = {
               }
             }`,
             output: "/rss.xml",
-            title: "Gatsby Starter Blog RSS Feed",
+            title: "Boris D. Teoharov's Blog RSS Feed",
           },
         ],
       },
@@ -111,15 +111,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `Gatsby`,
+        name: `Boris D. Teoharov's Blog`,
+        short_name: `BDTBlog`,
         start_url: `/`,
         background_color: `#ffffff`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: 'src/images/bd-icon@4x.png'
       },
     },
   ],
