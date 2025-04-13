0abd211829136e4773b25c56cd72c68e19abc222
Boris D. Teoharov <boristeoharov@gmail.com>
2025-04-14 02:55:35 +0300

    Add modern copy button functionality for code blocks; enhance styling and responsiveness

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -11,9 +11,19 @@ import "prismjs/themes/prism.css"
 // FontAwesome configuration
 import "@fortawesome/fontawesome-svg-core/styles.css"
 import { config as faConfig } from "@fortawesome/fontawesome-svg-core"
+
+// Import copy button functionality
+import { addCopyButtons } from "./src/components/code-copy-button"
+
+// Configure Font Awesome
 faConfig.autoAddCss = false
 
 // Automatically reload the page when a new service worker is available
 export const onServiceWorkerUpdateReady = () => {
   window.location.reload()
+}
+
+// Add copy buttons to code blocks after the page is loaded
+export const onRouteUpdate = () => {
+  addCopyButtons()
 }
